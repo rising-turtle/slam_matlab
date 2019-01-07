acaac41389f665def8f49873d39b20e1724aec2a
He Zhang <hxzhang1@ualr.edu>
2019-01-07 13:46:16 -0500

    update result

--- plot_traj/robocane_exp/plot_time_comp.m
+++ plot_traj/robocane_exp/plot_time_comp.m
@@ -19,21 +19,22 @@
 r = 100;
 
 rng default % For reproducibility 
-% dvio = abs(random('normal', 4.63, 2.98, r, 1));
-% rovio = abs(random('normal', 11.35, 2.17, r, 1)); 
-% okvis = abs(random('normal', 22.54, 24.76, r, 1)); 
+sdvio = abs(random('normal', 4.63, 2.98, r, 1));
+rovio = abs(random('normal', 11.35, 2.17, r, 1)); 
+okvis = abs(random('normal', 22.54, 24.76, r, 1)); 
 % vins_mono = abs(random('normal', 35.35, 15.2, r, 1));
-% vins_mono_tc = abs(random('normal', 35.76, 15.14, r, 1));
+vins_mono_tc = abs(random('normal', 35.76, 15.14, r, 1));
 
-dvio = abs(random('normal', 19.41, 4.23, r, 1));
-rovio = abs(random('normal', 30.26, 5.16, r, 1)); 
-okvis = random('normal', 49.02, 24.21, r, 1); 
-vins_mono = random('normal', 113.55, 72.6, r, 1);
-vins_mono_tc = random('normal', 113.18, 75.86, r, 1);
+% sdvio = abs(random('normal', 19.41, 4.23, r, 1));
+% rovio = abs(random('normal', 30.26, 5.16, r, 1)); 
+% okvis = random('normal', 49.02, 24.21, r, 1); 
+% % vins_mono = random('normal', 113.55, 72.6, r, 1);
+% vins_mono_tc = random('normal', 113.18, 75.86, r, 1);
 
-
-x = [dvio rovio okvis vins_mono vins_mono_tc]; 
-hh = boxplot(x, 'Colors', 'rmcgb'); 
+% x = [dvio rovio okvis vins_mono vins_mono_tc]; 
+x = [sdvio rovio okvis vins_mono_tc]; 
+% hh = boxplot(x, 'Colors', 'rmcgb'); 
+hh = boxplot(x, 'Colors', 'rmcg'); 
 set(hh, {'linew'}, {2});
 % boxplot(dvio, 'colors', 'r');
 % hold on; 
